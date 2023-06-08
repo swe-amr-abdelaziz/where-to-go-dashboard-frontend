8906c9dd72e00d5af6517190f77909c63d00db6a
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-08 22:23:23 +0300

    Added table pagination in customers

--- src/Axios.js
+++ src/Axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:8001',
   headers: {
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODA3MzNhMzVjNmU3MjQ4NTI4ZGJhYSIsInJvbGUiOiJFbXBsb3llZSIsImlhdCI6MTY4NjE0MDAyMCwiZXhwIjoxNjkzOTE2MDIwfQ.BRqYM_3v5yBUgEr8UPHuHN7vm_tDKO4SkOYRPkItHko',
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODA3MzNhMzVjNmU3MjQ4NTI4ZGJhYSIsInJvbGUiOiJFbXBsb3llZSIsImlhdCI6MTY4NjIwNzg0MCwiZXhwIjoxNjkzOTgzODQwfQ.WeA8Zf2yhtUrJHcwWOueWCDY-ABqGMUsYg55jkHEZ-0',
     'Content-Type': 'application/json',
   },
 })

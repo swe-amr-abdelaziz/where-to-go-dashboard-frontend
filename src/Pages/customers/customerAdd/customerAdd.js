e6ba4c7762c210e2debe7d3839a5c51559d3ed64
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-11 19:26:10 +0300

    Added Address and phone number validation in customers add

--- src/Pages/customers/customerAdd/customerAdd.js
+++ src/Pages/customers/customerAdd/customerAdd.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
   CButton,
   CCard,
@@ -7,18 +7,38 @@ import {
   CForm,
   CFormInput,
   CFormLabel,
+  CFormSelect,
   CInputGroup,
   CInputGroupText,
   CRow,
 } from '@coreui/react'
-import { useDispatch } from 'react-redux'
-import { createCustomer } from '../../../Redux/CustomerSlice/customerSlice'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { createCustomer } from '../../../Redux/CustomerSlice/customerSlice'
+import { getCountries, getStates, getCities } from '../../../Redux/LocationSlice/locationSlice'
+import phoneCodes from '../../../assets/js/phoneCodes'
+
+const RandExp = require('randexp')
 
 const CustomerAdd = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
   const [validated, setValidated] = useState(false)
+  const [phoneExample, setPhoneExample] = useState('')
+  const [phoneRegex, setPhoneRegex] = useState(phoneCodes[49].regex)
+
+  const countries = useSelector((state) => state.location.countries)
+  const states = useSelector((state) => state.location.states)
+  const cities = useSelector((state) => state.location.cities)
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      event.stopPropagation()
+      handleSubmit(event)
+    }
+  }
 
   const [formData, setFormData] = useState({
     firstName: '',
@@ -26,22 +46,61 @@ const CustomerAdd = () => {
     email: '',
     password: '',
     street: '',
+    country: '',
+    state: '',
+    city: '',
+    zip: '',
+    phoneNumber: '',
+    dateOfBirth: '',
   })
+
+  useEffect(() => {
+    dispatch(getCountries())
+  }, [])
+
+  useEffect(() => {
+    const form = document.getElementById('customerAddForm')
+    form.addEventListener('keydown', handleKeyDown)
+    return () => {
+      form.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [formData])
+
+  useEffect(() => {
+    if (formData.country) {
+      dispatch(getStates({ country: formData.country }))
+    }
+  }, [formData.country])
+
+  useEffect(() => {
+    if (formData.country && formData.state) {
+      dispatch(getCities({ country: formData.country, state: formData.state }))
+    }
+  }, [formData.state])
+
+  useEffect(() => {
+    const randexp = new RandExp(new RegExp(phoneRegex))
+    setPhoneExample(randexp.gen())
+  }, [phoneRegex])
 
   const regexPatterns = {
     firstName: '^[A-Za-z]+$',
     lastName: '^[A-Za-z]+$',
     password: '^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$',
-    city: '^[A-Za-z\\s ]+$',
-    zip: '^\\d{5}$',
+    zip: '^(\\d{5}(?:[-\\s]\\d{4})?)?$',
   }
 
   const handleInputChange = (event) => {
+    console.log(cities)
     const { name, value } = event.target
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }))
+  }
+
+  const handlePhoneInputChange = (event) => {
+    setPhoneRegex(event.target.value)
   }
 
   const handleSubmit = (event) => {
@@ -51,6 +110,9 @@ const CustomerAdd = () => {
       event.stopPropagation()
     }
     if (form.checkValidity() === true) {
+      if (formData.dateOfBirth === '') {
+        delete formData.dateOfBirth
+      }
       dispatch(createCustomer(formData)).then((res) => {
         navigate('/customers')
       })
@@ -69,6 +131,7 @@ const CustomerAdd = () => {
           </CCol>
         </CRow>
         <CForm
+          id="customerAddForm"
           className="row g-3 needs-validation"
           noValidate
           validated={validated}
@@ -99,6 +162,7 @@ const CustomerAdd = () => {
               value={formData.lastName}
               onChange={handleInputChange}
               required
+              className="mt-3 mt-md-0"
             />
           </CCol>
           <CCol md={12}>
@@ -144,43 +208,105 @@ const CustomerAdd = () => {
             <CFormInput
               type="text"
               placeholder="Street"
-              feedbackInvalid="Enter a valid street"
               name="street"
               id="street"
               value={formData.street}
               onChange={handleInputChange}
             />
           </CCol>
-          {/* <CCol md={6}>
-            <CFormInput
-              type="text"
-              aria-describedby="validationCustom03Feedback"
-              feedbackInvalid="Please provide a valid city."
-              id="validationCustom03"
-              label="City"
-              required
-            />
-          </CCol>
-          <CCol md={3}>
+          <CCol md={6} lg={3}>
             <CFormSelect
-              aria-describedby="validationCustom04Feedback"
-              feedbackInvalid="Please select a valid state."
-              id="validationCustom04"
-              label="State"
-              required
+              name="country"
+              id="country"
+              value={formData.country}
+              onChange={handleInputChange}
             >
-              <option disabled>Choose...</option>
-              <option>...</option>
+              <option defaultValue="">Country</option>
+              {countries.map((country) => (
+                <option key={country.Iso3} value={country.name}>
+                  {country.name}
+                </option>
+              ))}
             </CFormSelect>
           </CCol>
-          <CCol md={3}>
+          <CCol md={6} lg={3}>
+            <CFormSelect
+              name="state"
+              id="state"
+              value={formData.state}
+              onChange={handleInputChange}
+            >
+              <option defaultValue="">State</option>
+              {states.map((state) => (
+                <option key={state.state_code} value={state.name}>
+                  {state.name}
+                </option>
+              ))}
+            </CFormSelect>
+          </CCol>
+          <CCol md={6} lg={3}>
+            <CFormSelect name="city" id="city" value={formData.city} onChange={handleInputChange}>
+              <option defaultValue="">City</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </CFormSelect>
+          </CCol>
+          <CCol md={6} lg={3}>
             <CFormInput
               type="text"
-              aria-describedby="validationCustom05Feedback"
-              feedbackInvalid="Please provide a valid zip."
-              id="validationCustom05"
-              label="Zip"
-              required
+              placeholder="eg. 12345 or 12345-6789"
+              feedbackInvalid="Enter a valid zip code"
+              name="zip"
+              id="zip"
+              pattern={regexPatterns.zip}
+              value={formData.zip}
+              onChange={handleInputChange}
+            />
+          </CCol>
+          <CFormLabel htmlFor="phoneCode">Phone</CFormLabel>
+          <CCol md={6} className="mt-0">
+            <CFormSelect
+              name="phoneCode"
+              id="phoneCode"
+              value={phoneRegex}
+              onChange={handlePhoneInputChange}
+            >
+              {phoneCodes.map((item, index) => (
+                <option key={`code_${index}`} value={item.regex}>
+                  {`${item.country} (${item.code})`}
+                </option>
+              ))}
+            </CFormSelect>
+          </CCol>
+          <CCol md={6} className="mt-0">
+            <CInputGroup>
+              <CInputGroupText>
+                <i className="pi pi-phone"></i>
+              </CInputGroupText>
+              <CFormInput
+                type="text"
+                placeholder={`eg. ${phoneExample}`}
+                feedbackInvalid={`Enter a valid phone number (eg. ${phoneExample})`}
+                name="phoneNumber"
+                id="phoneNumber"
+                pattern={phoneRegex}
+                value={formData.phoneNumber}
+                onChange={handleInputChange}
+                className="input-group-custom mt-3 mt-md-0"
+              />
+            </CInputGroup>
+          </CCol>
+          {/* <CCol md={6}>
+            <CFormInput
+              type="date"
+              name="dateOfBirth"
+              label="Date of Birth"
+              id="dateOfBirth"
+              value={formData.dateOfBirth}
+              onChange={handleInputChange}
             />
           </CCol> */}
           <CCol xs={12} className="d-flex justify-content-end mt-5">

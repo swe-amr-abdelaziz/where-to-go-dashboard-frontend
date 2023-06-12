22e7d707826c13fb8c2127dcc322994bd2322bc2
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-12 09:59:22 +0300

    Fixed addCustomer

--- src/Axios.js
+++ src/Axios.js
@@ -4,9 +4,19 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:8001',
   headers: {
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODA3MzNhMzVjNmU3MjQ4NTI4ZGJhYSIsInJvbGUiOiJFbXBsb3llZSIsImlhdCI6MTY4NjIzMzM3OCwiZXhwIjoxNjk0MDA5Mzc4fQ.H7SiXzH7DhhpeR3yxwOVJhZLaRv87njT5r-lzRuvUhM',
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODA3MzNhMzVjNmU3MjQ4NTI4ZGJhYSIsInJvbGUiOiJFbXBsb3llZSIsImlhdCI6MTY4NjUwMjAxMSwiZXhwIjoxNjk0Mjc4MDExfQ.HWSFmw_Pp7DYJHrX2Z-Wny0-aYzycq8v65AjYRAzoPk',
     'Content-Type': 'application/json',
   },
 })
 
+const axiosInstanceFormData = axios.create({
+  baseURL: 'http://localhost:8001',
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODA3MzNhMzVjNmU3MjQ4NTI4ZGJhYSIsInJvbGUiOiJFbXBsb3llZSIsImlhdCI6MTY4NjUwMjAxMSwiZXhwIjoxNjk0Mjc4MDExfQ.HWSFmw_Pp7DYJHrX2Z-Wny0-aYzycq8v65AjYRAzoPk',
+    'Content-Type': 'multipart/form-data',
+  },
+})
+
+export { axiosInstance, axiosInstanceFormData }
 export default axiosInstance
